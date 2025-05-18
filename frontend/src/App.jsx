a7911f5c2a98d5d3a077c166e23beb811aca0d75
C0deNe0 <access.naveenhiremath@gmail.com>
2025-05-19 02:56:02 +0530

    inital setup of crudApi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+import Tablelist from "./components/Tablelist"
+
+function App() {
+
+  return (
+    <>      
+      <Navbar />
+      <Tablelist />
+      {/* <div className="font-bold font-mono ml-140 mt-30 ">hello</div> */}
+    </>
+  )
+}
+
+export default App
