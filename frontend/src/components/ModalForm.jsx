737bcc28c6cbf82283b1b30835229fd29f97d552
C0deNe0 <access.naveenhiremath@gmail.com>
2025-05-27 22:43:59 +0530

    completed with frontend

--- frontend/src/components/ModalForm.jsx
+++ frontend/src/components/ModalForm.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ModalForm = ({ isOpen, onClose, mode }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [status, setStatus] = useState(false);
+  const [rate, setRate] = useState('');
+  const [job,setJob] = useState('');
+
+  const handleStatusChange = (e)=>{
+    setStatus(e.target.value == 'Active'); //set status as boolean
+  }
+
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    onClose()
+  }
+
   return (
     <>
       <dialog id="my_modal_3" className="modal " open={isOpen}>
@@ -9,26 +24,26 @@ const ModalForm = ({ isOpen, onClose, mode }) => {
             {" "}
             {mode === "edit" ? "Edit client" : "Client Detail"}
           </h3>
-          <form method="dialog"  >
+          <form method="dialog" onSubmit={handleSubmit}  >
             <label className="input file-input-accent my-4 w-full flex items-center gap-2 ">
               Name 
-              <input type="text" className="grow" placeholder="Add Names" />
+              <input type="text" className="grow" placeholder="Add Names" value={name} onChange={(e)=>{setName(e.target.value)}} />
             </label>
             <label className="input file-input-accent my-4 flex  w-full items-center gap-2">
               Email
-              <input type="text" className="grow" placeholder="Add Email" />
+              <input type="text" className="grow" placeholder="Add Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
             </label>
             <label className="input file-input-accent w-full flex my-4 items-center gap-2">
-              Jod
-              <input type="text" className="grow" placeholder="Add Jod" />
+              Job
+              <input type="text" className="grow" placeholder="Add Jod"  value={job} onChange={(e)=>{setJob(e.target.value)}}/>
             </label>
 
             <div className="flex mb-4 justify-between">
               <label className="input input-bordered mr-4 flex items-center gap-2">
                 Rate
-                <input type="text" className="grow" placeholder="something" />
+                <input type="text" className="grow" placeholder="something" value={rate} onChange={(e)=>{setRate(e.target.value)}} />
               </label>
-              <select className="select select-bordered w-full  max-w-xs">
+              <select value={status ? 'Active' : 'Inactive'} className="select select-bordered w-full  max-w-xs" onChange={handleStatusChange}>
                   <option >InActive</option>
                   <option >Active</option>
 
