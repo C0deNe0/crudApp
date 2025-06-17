b21953c7ea25011e1be80b35fb23387a15034b8d
C0deNe0 <access.naveenhiremath@gmail.com>
2025-06-17 11:38:26 +0530

    setting up server:

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config()
+const app = express();
+
+
+const PORT = process.env.PORT ?? 3000;
+app.get("/",(req,res)=>{
+    res.send("root route")
+})
+// process.exit(1);
+
+app.listen(PORT,()=>{
+     console.log(`The server is running on port ${PORT}`);
+     
+}
+    )
